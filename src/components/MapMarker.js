7beef91859c4e0e4d8151f32a5b19a6c19dc758f
Stephanie Oliveira <stearruda@gmail.com>
2018-08-07 15:16:08 +0200

    feat: add image to indicate data from Foursquare and styles on InfoWindow

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Marker, InfoWindow } from 'react-google-maps'
 import ChristmasIcon from '../images/gingerbread-man-48.png'
+import PoweredByFoursquare from '../images/Powered-by-Foursquare-full-color-300.png'
 
 class MapMarker extends Component {
   state = {}
@@ -14,23 +15,29 @@ class MapMarker extends Component {
   }
 
   render() {
+    // Initially the Markers have no animations or InfoWindow
     let animation = null
     let infoWindow = null
+
+    // When a location is clicked on the Location List
+    // then the Marker will receive the animation and InfoWindow
     if(this.props.isSelected === true){
       animation = 1
 
-      let infoWindowText = this.props.location.title
       let numberOfLikes = '...'
       if(this.props.clickedLocationLikes !== null){
         numberOfLikes = this.props.clickedLocationLikes
       }
 
-      infoWindow =  (<InfoWindow onCloseClick={() => this.handleInfoWindowOnClose()}>
-            <div>
-             <h3>{infoWindowText}</h3>
-             <p>Number of Likes on Foursquare: {numberOfLikes}</p>
-            </div>
-         </InfoWindow>)
+      infoWindow = (
+        <InfoWindow onCloseClick={() => this.handleInfoWindowOnClose()}>
+          <div className='infowindow-container'>
+            <h3>{this.props.location.title}</h3>
+            <p>Likes of this place: {numberOfLikes}</p>
+            <img src={PoweredByFoursquare}/>
+          </div>
+        </InfoWindow>
+      )
     }
 
     return(
